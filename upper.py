aa5b78d9353dda2eebc4e47fca3838a15f18f728
binarioGH <lacuentafalsadediegojonas@gmail.com>
2019-12-17 22:09:38 -0800

    Danika is the best waifu

--- upper.py
+++ upper.py
@@ -16,3 +16,27 @@ def transform(text, alphabet, add):
 			char = str(chr(char))
 		new_text += char
 	return new_text
+
+def title(text):
+	text = text.split()
+	
+	windex = 0
+	for word in text:
+		word = list(word)
+		index = -1
+		alpha = True
+		for letter in word:
+			index += 1
+			if not letter.isalpha():
+				continue
+			else:			
+				if alpha:
+					alpha = False
+					word[index] = upper(letter)
+				else:
+					word[index] = lower(letter)
+				
+		word = "".join(word)
+		text[windex] = word
+		windex += 1
+	return " ".join(text)
